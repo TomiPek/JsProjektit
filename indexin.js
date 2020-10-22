a8cc010c6baba1f30082032e5e3e5c64c5b7da74
TomiPek <72078799+TomiPek@users.noreply.github.com>
2020-10-22 13:26:46 +0300

    päivitetty kommentit

--- indexin.js
+++ indexin.js
@@ -1,16 +1,19 @@
 function uusiTeht() {
-
+  // Ensimmäisen taskin lisäyksen jälkeen saatetaan näkyville viesti miten poistetaan taskeja listalta.
   document.getElementById("otsikko").style.display = "block";
+
+  // Määritellään muuttujat listan tekoa ja käyttäjän syötteen talteen ottoa varten.
   var lista = document.createElement("li");
   var teksti = document.getElementById("textKentta").value;
   var tp = document.createTextNode(teksti);
   lista.appendChild(tp);
   
+  // Tyhjästä tekstikentästä annetaan ilmoitus alert viestillä ja muutetaan tekstikentän väriä
   if (teksti == "") {
     alert("Text field is empty!");
     document.getElementById("textKentta").style.backgroundColor = "red";
   }
-
+  // Onnistuneen käyttäjä syötteen jälkeen lisätään käyttäjän syöte listaan.
  else {
     document.getElementById("udlista").appendChild(lista);
     document.getElementById("textKentta").style.background = "";
@@ -19,7 +22,7 @@ function uusiTeht() {
   document.getElementById("textKentta").value = "";
 }
 
-
+  // Evenlistener jonka avulla käyttäjä voi klikkaamalla poistaa taskeja.
 document.getElementById("udlista").addEventListener("click",function(e) {
   var poista = e.target;
   if (poista.tagName.toUpperCase() == "LI") {
